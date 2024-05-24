7bbc6d55569e4c5e2c1ec3545d938834eb35716f
Anna Markevych <anyaskarlet@gmail.com>
2024-05-24 14:40:16 +0100

    Added top-bar for menu, theme-switcher

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const page = document.documentElement;
+const switcher = document.querySelector('.theme__switcher');
+
+switcher.addEventListener('click', (a,b,c) => {
+  if (page.classList.contains('page--theme--blue')) {
+    page.classList.remove('page--theme--blue');
+  } else {
+    page.classList.add('page--theme--blue');
+  }
+});
+
